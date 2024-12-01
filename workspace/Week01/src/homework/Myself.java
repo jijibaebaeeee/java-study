f82c1b5199560c7908bd3bcbbee1602d4a30ee9e
jijibaebaeeee <isnna1@naver.com>
2024-12-01 20:07:41 +0900

    패키지 이름 혼동하여 의미없는 수정 후 재커밋

---
+++ workspace/Week01/src/homework/Myself.java
@@ -0,0 +1,17 @@
+package homework;
+
+public class Myself {
+	public static void main(String[] args) {
+		 int age = 21;
+		 
+		 if (age >= 20) {
+			 System.out.println("성인입니다");
+		 }
+		 
+		 if (age < 20) {
+			 System.out.println("미성년자입니다");
+		 }
+		 
+	}
+
+}
---
+++ workspace/Week01/src/homework/Printlnfo.java
@@ -0,0 +1,8 @@
+package homework; // 이 클래스는 homework 라는 패키지에 속함
+
+public class Printlnfo { // 클래스의 중괄호 시작영역
+	public static void main(String[] args) { // main 메소드의 중괄호 시작영역
+		System.out.println(" 이름 : 배지수, 나이 : 26"); // 이름과 나이의 값을 출력하고 줄바꿈 한다
+	}  // main 메소드의 중괄호 끝영역
+
+} // 클래스의 중괄호 끝영역
