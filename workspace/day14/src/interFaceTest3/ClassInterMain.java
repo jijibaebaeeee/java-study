8e758512d37045d6b61f03804af6235d63410c8f
jijibaebaeeee <isnna1@naver.com>
2024-12-13 20:51:15 +0900

    interface의 default 메소드와 Class의 메소드가 동일한 경우 Class의 메서드에 public을 붙여서 충돌 해결

--- workspace/day14/src/interFaceTest3/ClassInterMain.java
+++ workspace/day14/src/interFaceTest3/ClassInterMain.java
@@ -1,6 +1,8 @@
 package interFaceTest3;
 
 class ClassInterMain extends Class1 implements Inter1 {
+
+
 //  The inherited method Class1.printText() 
 //  cannot hide the public abstract method in Inter1
 //  인터페이스의 메소드와 클래스 메소드 간의 충돌 문제 발생
@@ -9,5 +11,6 @@ class ClassInterMain extends Class1 implements Inter1 {
   //Class1과 Inter1 양쪽에 printText()메소드가 존재하지만
   //오류발생가 발생하지 않는 이유는 구현하는 인터페이스보다 상속받는 부모 클래스의
   //우선순위가 더 높기 때문이다
+	
 
 }
--- workspace/day14/src/interFaceTest3/Inter1.java
+++ workspace/day14/src/interFaceTest3/Inter1.java
@@ -1,8 +1,7 @@
 package interFaceTest3;
 //인터페이스의 디폴트메소드와 클래스 메소드가 동일한 경우
 public interface Inter1 {
-	default void printText() {
-		System.out.println("Inter1의 printText 메서드 출력");
-	}
-
+	void printText(); //{
+//		System.out.println("Inter1의 printText 메서드 출력");
+//	}
 }
