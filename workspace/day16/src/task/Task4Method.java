ff70ed9e2d4dc07facd18eb18a0a78bb71fe972e
jijibaebaeeee <isnna1@naver.com>
2024-12-13 20:49:03 +0900

    hasNextInt() 메소드와 nextInt() 메소드의 동시 사용시  버퍼의 남아있는 문자를 소모함 학습

--- workspace/day16/src/task/Task4Method.java
+++ workspace/day16/src/task/Task4Method.java
@@ -5,17 +5,19 @@ public class Task4Method {
 	
 	void mapping(Scanner sc) throws TaskException{
 		System.out.println("정수 입력 : ");
-		
 		//Returns true if the next token in this scanner's input can be interpreted as an int value 
 		//in the default radix using the nextInt() method. 
 		//The scanner does not advance past any input.
 		if(!(sc.hasNextInt())) { // <-- 정수를 입력받지 않았다면!
+			
 			throw new TaskException("숫자가 아닌 문자열이 입력되었습니다.");
+			
 		}
-		
 		num = sc.nextInt();
 
-		
+		System.out.println(num);
+//		sc.next();
+				
 		if((num % 3 == 0 && num % 5 == 0)) {
 			System.out.println("Fizz");
 		}else if(num % 5 == 0) {
